53ce985de9bb3476718a979659f3f18877115bad
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-09-21 14:46:05 +0700

    add enum EuroStandart

---
+++ DealerShip2109/DealerShip2109/Data/Enums/EuroStandart.cs
@@ -0,0 +1,13 @@
+﻿namespace DealerShip2109.Data.Enums
+{
+    public enum EuroStandart
+    {
+        NoStandart,
+        Euro1,
+        Euro2,
+        Euro3,
+        Euro4,
+        Euro5,
+        Euro6
+    }
+}
