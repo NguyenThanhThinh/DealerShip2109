2f0d3e99f82a84065bf5f41d40832d505157c472
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-09-21 14:41:58 +0700

    add enum bodytype

---
+++ DealerShip2109/DealerShip2109/Data/Enums/BodyType.cs
@@ -0,0 +1,13 @@
+﻿namespace DealerShip2109.Data.Enums
+{
+    public enum BodyType
+    {
+        Coupe,
+        Sedan,
+        Hatchback,
+        Minivan,
+        Van,
+        Suv,
+        Supercar
+    }
+}
