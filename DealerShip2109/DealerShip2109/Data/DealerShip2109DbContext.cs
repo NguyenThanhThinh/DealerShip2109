ca655ad8e7e3ba579e783f301a63403803e20957
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-09-21 15:12:33 +0700

    add dbcontext

---
+++ DealerShip2109/DealerShip2109/Data/DealerShip2109DbContext.cs
@@ -0,0 +1,31 @@
+﻿using DealerShip2109.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DealerShip2109.Data
+{
+    public class DealerShip2109DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DealerShip2109DbContext(DbContextOptions<DealerShip2109DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Car>()
+                .HasMany(c => c.Images)
+                .WithOne(i => i.Car)
+                .HasForeignKey(i => i.CarId);
+
+            base.OnModelCreating(builder);
+
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
