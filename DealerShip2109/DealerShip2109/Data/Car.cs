fbe2e088a2a69b061498fd28744e28527f4f8423
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-09-21 15:00:33 +0700

    add entities

---
+++ DealerShip2109/DealerShip2109/Data/Car.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using DealerShip2109.Data.Enums;
+
+namespace DealerShip2109.Data
+{
+    public class Car
+    {
+        public Car()
+        {
+            Images = new HashSet<Image>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Model { get; set; }
+
+        [Range(1950, 2100)]
+        [Display(Name = "Year of production")]
+        public short YearOfProduction { get; set; }
+
+        public BodyType BodyType { get; set; }
+
+        public Condition Condition { get; set; }
+
+        [Display(Name = "Type of transmission")]
+        public TypeOfTransmission TypeOfTransmission { get; set; }
+
+        [Display(Name = "Euro standart")] public EuroStandart EuroStandart { get; set; }
+
+        [Display(Name = "Engine type")] public EngineType EngineType { get; set; }
+
+        [Range(0, int.MaxValue)]
+        [Display(Name = "Travelled distance")]
+        public int TravelledDistance { get; set; }
+
+        [Range(0, short.MaxValue)]
+        [Display(Name = "Horsepower")]
+        public short HorsePower { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Color { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(700)]
+        [Display(Name = "Description")]
+        public string SaleDescription { get; set; }
+
+        [Range(0, double.MaxValue)] public decimal Price { get; set; }
+
+        public ICollection<Image> Images { get; set; }
+    }
+}
---
+++ DealerShip2109/DealerShip2109/Data/Image.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DealerShip2109.Data
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Url]
+        public string ImageUrl { get; set; }
+
+        public int CarId { get; set; }
+
+        public Car Car { get; set; }
+    }
+}
