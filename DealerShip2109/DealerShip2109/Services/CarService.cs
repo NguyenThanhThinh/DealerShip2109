fc4abc1c89963b157e3edf89071bcca97d6be7ed
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-09-21 16:03:04 +0700

    add method to ICarservice

--- DealerShip2109/DealerShip2109/Services/CarService.cs
+++ DealerShip2109/DealerShip2109/Services/CarService.cs
@@ -1,11 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DealerShip2109.Data;
+using DealerShip2109.ViewModels.Cars;
+using Microsoft.AspNetCore.Http;
 
 namespace DealerShip2109.Services
 {
     public class CarService:ICarService
     {
+        public IEnumerable<Car> All(string sort, string searchQuery, int page = 1, int pageSize = 6)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendEMail(BuyCarFormModel personInfo, Car carToBuy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Car addCarFormModel, ICollection<IFormFile> images)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id, Car editCarFormModel, ICollection<IFormFile> images)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int IdToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePhoto(int photoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> SearchCars(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DealerShip2109/DealerShip2109/Services/ICarService.cs
+++ DealerShip2109/DealerShip2109/Services/ICarService.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using DealerShip2109.Data;
+using DealerShip2109.ViewModels.Cars;
+using Microsoft.AspNetCore.Http;
 
 namespace DealerShip2109.Services
 {
     public interface ICarService
     {
+        IEnumerable<Car> All(string sort, string searchQuery, int page = 1, int pageSize = 6);
+
+        void SendEMail(BuyCarFormModel personInfo, Car carToBuy);
+
+        void Add(Car addCarFormModel, ICollection<IFormFile> images);
+
+        void Edit(int id, Car editCarFormModel, ICollection<IFormFile> images);
+
+        void Delete(int IdToRemove);
+
+        void DeletePhoto(int photoId);
+
+        bool Exists(int id);
+
+        Car FindById(int id);
+
+        IEnumerable<Car> SearchCars(string searchQuery);
     }
 }
